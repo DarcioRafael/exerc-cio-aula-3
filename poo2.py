7a4d6a7821f28eada482317e27fcff2e27709a79
DarcioRafael <liminhadarcioo@gmail.com>
2024-10-02 21:32:19 -0300

    Add files via upload

---
+++ poo2.py
@@ -0,0 +1,26 @@
+
+
+#numero1 = int (input(' Digite o numero: '))
+#numero2 = int (input(' Digite o numero: '))
+#resultado = numero1 + numero2
+
+#print (f' o numero {numero1} e {numero2} o resultado e {resultado}')
+
+
+#nome = input (' insira o primeiro nome: ')
+#sobrenome = input (' insira o sobrenome: ')
+
+#print ( nome, sobrenome)
+
+
+#nota1 = float(input(' digite nota 1: '))
+#nota2 = float(input (' digite nota 2: '))
+#nota3 = float(input(' digite nota 3: '))
+#media = ( nota1 + nota2 + nota3 )/3
+
+#print( ' a media' + media)
+
+#largura = float(input (' digite a largura'))
+#altura = float( input(' digite a altura'))
+
+#print( 'a area do triangulo é' + (largura*altura))
